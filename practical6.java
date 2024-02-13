d90b6944e8be804528755ce64d751c8adfd4f1b8
Darshana Dave <darshanadave10c@gmail.com>
2024-02-13 15:23:15 +0530

    java updated

---
+++ practical6.java
@@ -0,0 +1,20 @@
+public class practical6 {
+    public class shape{
+        double area(float radius)
+        {
+            return 3.14*radius*radius;
+        }
+         double area(float length, float width)
+         {
+            return length*width;
+         }
+
+    }
+    public static void main(String[] args)
+    {
+        shape s1=new shape();
+        
+        shape s2=new shape();
+
+    }
+}
